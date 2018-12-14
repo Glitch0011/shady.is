a8d3b7c8e219f54d80c817eae2b77303718cd4e1
Tom Bonner <tom+github@bonner.is>
2018-12-14 20:12:03 +0000

    Updated all links to new path

--- site/Examples/Inline Register/InlineElement.js
+++ site/Examples/Inline Register/InlineElement.js
@@ -1,4 +1,4 @@
-import ShadyElement from "/node_modules/shady-components/shady.js"
+import ShadyElement from "/node_modules/shady-components/dist/shady.js"
 
 export class InlineElement extends ShadyElement {
 
